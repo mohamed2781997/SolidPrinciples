74aed0aeb37ee3be22e2b485b8c0222ab6bcb83c
magdy <mohammed2781997@gmail.com>
2025-01-27 23:02:14 +0200

    Liskov Principles Example2

--- SolidPrinciples/Program.cs
+++ SolidPrinciples/Program.cs
@@ -2,7 +2,7 @@
 using SolidPrinciples.LiskovSubstitutionPrinciple;
 using SolidPrinciples.OpenClosedPrinciples;
 using SolidPrinciples.SingleResponsibilityPrinciples;
-
+using System.Diagnostics.Metrics;
 namespace SolidPrinciples
 {
     public  class Program
@@ -11,37 +11,48 @@ namespace SolidPrinciples
         {
 
             #region SingleResponsibility
-            Employee employee = new Employee("John Doe", 160, 25);
+            //Employee employee = new Employee("John Doe", 160, 25);
 
-            CalculateSalaryEmployee calculateSalaryEmployee = new CalculateSalaryEmployee();
-
-
-            EmployeeReport report = new EmployeeReport();
-            report.Print(employee, calculateSalaryEmployee);
+            //CalculateSalaryEmployee calculateSalaryEmployee = new CalculateSalaryEmployee();
 
 
-            EmployeeDatabase database = new EmployeeDatabase();
-            database.Save(employee);
+            //EmployeeReport report = new EmployeeReport();
+            //report.Print(employee, calculateSalaryEmployee);
+
+
+            //EmployeeDatabase database = new EmployeeDatabase();
+            //database.Save(employee);
             #endregion
 
             #region OpenClosedPrinciples
-            Product product1 = new Product("Electronics", 1000);
-            Product product2 = new Product("Clothing", 500);
+            //Product product1 = new Product("Electronics", 1000);
+            //Product product2 = new Product("Clothing", 500);
 
-            DiscountCalculator discountCalculator1 = new DiscountCalculator(new ElectronicsDiscount());
-            DiscountCalculator discountCalculator2 = new DiscountCalculator(new ClothingDiscount());
+            //DiscountCalculator discountCalculator1 = new DiscountCalculator(new ElectronicsDiscount());
+            //DiscountCalculator discountCalculator2 = new DiscountCalculator(new ClothingDiscount());
 
-            Console.WriteLine($"Electronics Discount: {discountCalculator1.GetDiscount(product1)}");
-            Console.WriteLine($"Clothing Discount: {discountCalculator2.GetDiscount(product2)}");
+            //Console.WriteLine($"Electronics Discount: {discountCalculator1.GetDiscount(product1)}");
+            //Console.WriteLine($"Clothing Discount: {discountCalculator2.GetDiscount(product2)}");
             #endregion
 
 
             #region Liskov
-            Shape rectangle = new Rectangle(5, 10);
-            Shape square = new Square(5);
+            //Shape rectangle = new Rectangle(5, 10);
+            //Shape square = new Square(5);
 
-            Console.WriteLine($"Rectangle Area: {rectangle.Area()}");
-            Console.WriteLine($"Square Area: {square.Area()}"); 
+            //Console.WriteLine($"Rectangle Area: {rectangle.Area()}");
+            //Console.WriteLine($"Square Area: {square.Area()}");
+
+
+            //var account = new Before.FixedDepositAccount("Issam", 10_000);
+            //account.Withdraw(1000); 
+
+            var account = new After.CheckingAccount("Issam", 10_000);
+            account.Withdraw(1000);
+
+            //var account = new After.FixedDepositAccount("Issam", 10_000);
+            //account.Withdraw(1000); // won't work
+            Console.ReadKey();
             #endregion
 
         }
